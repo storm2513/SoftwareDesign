3f2c0e3ca8a9337df78cf787d8bb02a8d2b8a406
Maksim Shilov <storm25132513@gmail.com>
2018-09-30 14:17:28 +0300

    added IMEI with version

--- app/src/main/java/com/bsuirlabs/softwaredesign/MainActivity.kt
+++ app/src/main/java/com/bsuirlabs/softwaredesign/MainActivity.kt
@@ -1,12 +1,98 @@
 package com.bsuirlabs.softwaredesign
 
+import android.Manifest
+import android.Manifest.permission.READ_PHONE_STATE
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.content.pm.PackageManager.*
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v4.app.ActivityCompat
+import android.support.v4.app.ActivityCompat.checkSelfPermission
+import android.support.v7.app.AlertDialog
+import android.telephony.TelephonyManager
+import android.widget.TextView
+
+
+private const val PermissionsRequestReadPhoneState = 0
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (checkSelfPermission(this@MainActivity,
+                        READ_PHONE_STATE) != PERMISSION_GRANTED) {
+            showPermissionExplanationDialog();
+        } else {
+            setImei()
+        }
+        setProjectVersion()
+    }
+
+    private fun setProjectVersion(){
+        val version = findViewById<TextView>(R.id.projectVersion)
+        version.text = BuildConfig.VERSION_NAME
+    }
+
+    private fun requestReadPhoneStatePermission(){
+        ActivityCompat.requestPermissions(this@MainActivity,
+                arrayOf(Manifest.permission.READ_PHONE_STATE),
+                PermissionsRequestReadPhoneState)
+    }
+
+    private fun showPermissionExplanationDialog(){
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage(resources.getString(R.string.phone_calls_permission_explanation))
+                .setCancelable(false)
+                .setPositiveButton(resources.getString(R.string.ok)) { _, _ ->
+                    requestReadPhoneStatePermission()
+                }
+        val alert = builder.create()
+        alert.show()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            PermissionsRequestReadPhoneState -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    setImei()
+                } else {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(this@MainActivity,
+                                    Manifest.permission.READ_PHONE_STATE)) {
+                        showSnackbar()
+                    }
+                }
+                return
+            }
+        }
+    }
+
+    private fun showSnackbar(){
+        val snackbar = Snackbar.make(findViewById(R.id.imei),
+                resources.getString(R.string.dont_have_imei_permission), Snackbar.LENGTH_INDEFINITE)
+        snackbar.setAction(resources.getString(R.string.grant_permission)) {
+            snackbar.dismiss()
+            requestReadPhoneStatePermission()
+        }
+        snackbar.show()
+    }
+
+    @SuppressLint("HardwareIds")
+    private fun setImei() {
+        if (checkSelfPermission(this@MainActivity,
+                        READ_PHONE_STATE) == PERMISSION_GRANTED) {
+            val imeiTextView = findViewById<TextView>(R.id.imei)
+            val tel = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+            imeiTextView.text = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                                    tel.imei
+                                } else {
+                                    @Suppress("DEPRECATION")
+                                    tel.deviceId
+                                }
+        }
     }
 }
